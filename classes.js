66cd64c8254ad1409764e1c6791f40d1048b6ff0
Wangle17 <e.cookie313@gmail.com>
2026-05-28 08:00:35 +1000

    classes

---
+++ classes.js
@@ -0,0 +1,8 @@
+// classes.js
+const classesData = {
+  classes: [
+    { id: "eng2", name: "Engineering", emoji: "⚙️", colour: "#0f4c81", storageKey: "eng2_topics", unitsKey: "eng2_units" },
+    { id: "2026_12_mth",  name: "bMaths",       emoji: "📐", colour: "#1d4ed8", storageKey: "bmth_topics",  unitsKey: "bmth_units"  }
+  ]
+};
+
