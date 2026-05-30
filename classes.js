3d930e703cb7da3d44c27b274e296d9ea6fb1d3a
Wangle17 <e.cookie313@gmail.com>
2026-05-30 18:06:34 +1000

    updated

--- classes.js
+++ classes.js
@@ -2,7 +2,7 @@
 const classesData = {
   subjects: [
     { id: "2026-12A-mth",  name: "Maths", teacher: "H.Aldous", class: "A", emoji: "📐", colour: "#1d4ed8", storageKey: "2026-12A-mth_topics",  unitsKey: "2026-12A-mth_units"  },
-    { id: "2026-12A-eng2", name: "Engineering", teacher: "", class: "A", emoji: "⚙️", colour: "#0f4c81", storageKey: "2026-12A-eng2_topics", unitsKey: "2026-12A-eng2_units" }
+    { id: "2026-12A-eng2", name: "Engineering", teacher: "D.Brennan", class: "A", emoji: "⚙️", colour: "#0f4c81", storageKey: "2026-12A-eng2_topics", unitsKey: "2026-12A-eng2_units" }
   ]
 };
 
